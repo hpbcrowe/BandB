6a9bc793e4c1bfd314042fcf451fb8cde9d26654
Ben Crowe <benjamin.crowe@comcast.net>
2025-05-04 18:10:49 -0600

    first commit

---
+++ components/TopNav.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function TopNav() {
+    return (
+        <nav className="nav shadow p-2 justify-content-between mb-3">
+            <Link href="/" className='nav-link'>Beauty and Buckaroo</Link>
+            <div></div>
+        </nav>
+    )
+}
