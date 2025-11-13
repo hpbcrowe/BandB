fadf03728994612c0ab0eaebb7c1a5a333f53a29
Ben Crowe <benjamin.crowe@comcast.net>
2025-11-12 22:15:20 -0700

    fixed bootstrap for loading

--- app/loading.js
+++ app/loading.js
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div
-      className="d-flex justify-conent-center align-items-center vh-100 text-danger"
+      className="d-flex justify-content-center align-items-center vh-100 text-danger"
       suppressHydrationWarning
     >
       LOADING...
